c1451d4790974f26ae318695fb12e6757ed57488
KatyPreston <prestonkj@hotmail.com>
2018-04-13 15:23:03 +0100

    customer cash removal

--- pet_shop.rb
+++ pet_shop.rb
@@ -23,7 +23,7 @@ def stock_count(shop)
   shop[:pets].length
 end
 
-
+#this function works for both tests for breed
 def pets_by_breed(shop, type)
   pets = []
   for pet in shop[:pets]
@@ -32,4 +32,29 @@ def pets_by_breed(shop, type)
     end
   end
   return pets
+end
+
+
+# def find_pet_by_name(shop, pet)
+#    pet = shop[:pets]
+# end
+
+#the next one too
+
+# and this one is maybe linked
+
+# def remove_pet_by_name(shop, name)
+#   shop[:pets].delete(name)
+# end
+
+def add_pet_to_stock(shop, new_pet)
+  shop[:pets].push(new_pet)
+end
+
+def customer_cash(customers)
+  customers[:cash]
+end
+
+def remove_customer_cash(customer, amount)
+  customer[:cash] -= amount
 end
